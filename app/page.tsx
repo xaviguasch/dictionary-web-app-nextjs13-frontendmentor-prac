cf8c30fd004b918e0c73d814a13019cac623a1a6
Xavi Guasch <xaviguasch@gmail.com>
2023-12-15 16:22:46 +0100

    added basic custom css properties: fonts and colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { UrlWithStringQuery } from 'url'
 
-export default function Home() {
+async function getWord(word: string) {
+  const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+
+  return await res.json()
+}
+
+export default async function Home() {
+  const w = await getWord('armed')
+  console.log(w[0].word)
+
   return (
     <main className={styles.main}>
-      <h1>Hola</h1>
+      <h1>{w[0].word}</h1>
     </main>
   )
 }
